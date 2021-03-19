b4b176f629f729bf251a1ffbf19437e2ae9929a2
Administrator <Administrator@DESKTOP-CJ8TOD1>
2021-03-19 17:21:57 +0700

    add file to initial project

---
+++ PAKNAPI/Middle/CustomMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Middle
+{
+	public class CustomMiddleware : IMiddleware
+	{
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			context.Request.EnableBuffering();
+
+			using (var reader = new StreamReader(context.Request.Body, encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false, leaveOpen: true))
+			{
+				var body = await reader.ReadToEndAsync();
+				context.Items.Add("body", body);
+
+				context.Request.Body.Position = 0;
+			}
+
+			await next(context);
+		}
+	}
+}
