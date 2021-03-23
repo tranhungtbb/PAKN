6f26069e64e541b9c942e2527e3fc92a786379c9
mrRicardo <vinc.thanhquyen@gmail.com>
2021-03-23 18:21:36 +0700

    commit sy

---
+++ PAKNWEB/src/app/modules/system-management/components/email-setting/email-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-setting',
+  templateUrl: './email-setting.component.html',
+  styleUrls: ['./email-setting.component.css']
+})
+export class EmailSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- PAKNWEB/src/app/modules/system-management/system-management-routing.module.ts
+++ PAKNWEB/src/app/modules/system-management/system-management-routing.module.ts
@@ -1,34 +1,23 @@
 import { NgModule, Component } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
-import { SystemManagemenetComponent } from './system-managemenet.component'
-import { OrgnizationComponent } from './components/orgnization/orgnization.component'
-import { SystemConfigComponent } from './components/system-config/system-config.component'
-import { SystemLogComponent } from './components/system-log/system-log.component'
 import { RoleGuardService } from '../../guards/role-guard.service'
+import { SystemManagemenetComponent } from './system-managemenet.component'
+import { SystemLogComponent } from './components/system-log/system-log.component'
+import { EmailSettingComponent } from './components/email-setting/email-setting.component'
+import { SmsSettingComponent } from './components/sms-setting/sms-setting.component'
+import { GroupUserComponent } from './components/group-user/group-user.component'
+import { UserComponent } from './components/user/user.component'
 
 const routes: Routes = [
 	{
 		path: '',
 		component: SystemManagemenetComponent,
 		children: [
-			{
-				path: 'co-cau-to-chuc',
-				component: OrgnizationComponent,
-			},
-			{
-				path: 'nguoi-dung',
-				loadChildren: './components/users-management/users-management.module#UsersManagementModule',
-			},
-			{
-				path: 'vai-tro',
-				component: OrgnizationComponent,
-				//canActivate: [RoleGuardService],
-				//data: { role: 'A_II_0' }
-			},
-			{
-				path: 'support-management',
-				loadChildren: './components/support-management/support-management.module#SupportManagementModule',
-			},
+			{ path: 'cau-hinh-email', component: EmailSettingComponent },
+			{ path: 'cau-hinh-sms', component: SmsSettingComponent },
+			{ path: 'vai-tro', component: GroupUserComponent },
+			{ path: 'nguoi-dung', component: UserComponent },
+			{ path: 'lich-su-he-thong', component: SystemLogComponent },
 		],
 	},
 ]
