a2551d0231be22437d47ba92fd6e031d1bfe9823
mrRicardo <vinc.thanhquyen@gmail.com>
2021-03-23 18:02:06 +0700

    config link catalog

---
+++ PAKNWEB/src/app/modules/catalog-management/word-library/word-library.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-library',
+  templateUrl: './word-library.component.html',
+  styleUrls: ['./word-library.component.css']
+})
+export class WordLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
