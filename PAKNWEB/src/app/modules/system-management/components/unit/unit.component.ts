54daf22647fdbee63e074fb02f5e7e4776e59b63
duongth <thanhduong9613@gmail.com>
2021-03-24 10:23:51 +0700

    update

---
+++ PAKNWEB/src/app/modules/system-management/components/unit/unit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+declare var $: JQuery
+declare var jquery: JQuery
+@Component({
+	selector: 'app-unit',
+	templateUrl: './unit.component.html',
+	styleUrls: ['./unit.component.css'],
+})
+export class UnitComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
