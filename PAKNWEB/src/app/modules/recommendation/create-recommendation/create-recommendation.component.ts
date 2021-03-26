4a91e902134a95f5698c893f899d857ff7e12618
mrRicardo <vinc.thanhquyen@gmail.com>
2021-03-26 10:21:24 +0700

    add recommendation

---
+++ PAKNWEB/src/app/modules/recommendation/create-recommendation/create-recommendation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-recommendation',
+  templateUrl: './create-recommendation.component.html',
+  styleUrls: ['./create-recommendation.component.css']
+})
+export class CreateRecommendationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
