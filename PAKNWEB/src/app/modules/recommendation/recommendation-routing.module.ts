d812fc301b664a730c64d3b2638766a352a1539c
mrRicardo <vinc.thanhquyen@gmail.com>
2021-03-29 18:32:58 +0700

    add list recommendation

--- PAKNWEB/src/app/modules/recommendation/recommendation-routing.module.ts
+++ PAKNWEB/src/app/modules/recommendation/recommendation-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { CreateRecommendationComponent } from './create-recommendation/create-recommendation.component'
 import { ListGeneralComponent } from './list-general/list-general.component'
+import { ListReceiveApprovedComponent } from './list-receive-approved/list-receive-approved.component'
+import { ListReceiveDenyComponent } from './list-receive-deny/list-receive-deny.component'
+import { ListReceiveWaitComponent } from './list-receive-wait/list-receive-wait.component'
 import { RecommendationComponent } from './recommendation.component'
 
 const routes: Routes = [
@@ -9,8 +12,11 @@ const routes: Routes = [
 		path: '',
 		component: RecommendationComponent,
 		children: [
-			{ path: 'danh-sach-tong-hop', component: ListGeneralComponent },
 			{ path: 'them-moi/:id', component: CreateRecommendationComponent },
+			{ path: 'danh-sach-tong-hop', component: ListGeneralComponent },
+			{ path: 'cho-xu-ly', component: ListReceiveWaitComponent },
+			{ path: 'tiep-nhan-xu-ly', component: ListReceiveApprovedComponent },
+			{ path: 'tu-choi-tiep-nhan', component: ListReceiveDenyComponent },
 		],
 	},
 ]
