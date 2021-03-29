16842dab4ea02f16401c66d75d58392b599dcf7b
hien11198 <hien78361@nuce.edu.vn>
2021-03-29 14:54:29 +0700

    update departmentObj

---
+++ PAKNWEB/src/app/models/departmentObject.ts
@@ -0,0 +1,18 @@
+export class DepartmentObject {
+	constructor() {
+		this.id = 0
+		this.orderNumber = null
+		this.name = ''
+		this.code = ''
+		this.description = ''
+		this.isDeleted = false
+	}
+	id: number
+	orderNumber: number
+	name: string
+	code: string
+	description: string
+	isActived: boolean
+	isDeleted: boolean
+  departmentGroup: number
+}
