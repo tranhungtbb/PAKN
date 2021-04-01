7b5d63551336f7d8c7f75d70f1dde54f269b17f2
hien11198 <hien78361@nuce.edu.vn>
2021-04-01 15:44:20 +0700

    update

--- PAKNWEB/src/app/modules/catalog-management/department/department.component.ts
+++ PAKNWEB/src/app/modules/catalog-management/department/department.component.ts
@@ -74,7 +74,7 @@ export class DepartmentComponent implements OnInit {
       description: [this.model.description],
       isActived: [this.model.isActived, Validators.required],
       departmentGroupId: [this.model.departmentGroup, Validators.required],
-      email: ['', [Validators.required, Validators.pattern('^[a-z][a-z0-9_.]{3,32}@[a-z0-9]{2,}(.[a-z0-9]{2,4}){1,2}$')]], //Validators.pattern('^[a-z][a-z0-9_.]{5,32}@[a-z0-9]{2,}(.[a-z0-9]{2,4}){1,2}$')
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9_.][a-z0-9_.]{3,32}@[a-z0-9]{2,}(.[a-z0-9]{2,4}){1,2}$')]], //Validators.pattern('^[a-z][a-z0-9_.]{5,32}@[a-z0-9]{2,}(.[a-z0-9]{2,4}){1,2}$')
       phone: ['', [Validators.required, Validators.pattern('^(84|0[3|5|7|8|9])+([0-9]{8})$')]],
       address: [''],
       fax: ['']
