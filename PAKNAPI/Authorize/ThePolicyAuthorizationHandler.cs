af3534b50a61d8efc1e19ebcd36bc40fd731d6c4
Administrator <Administrator@DESKTOP-CJ8TOD1>
2021-04-03 12:37:09 +0700

    fix core

--- PAKNAPI/Authorize/ThePolicyAuthorizationHandler.cs
+++ PAKNAPI/Authorize/ThePolicyAuthorizationHandler.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing.Patterns;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
@@ -11,39 +9,56 @@ using System.Web.Http.Controllers;
 
 namespace PAKNAPI.Authorize
 {
-    public class ThePolicyAuthorizationHandler : AuthorizationHandler<ThePolicyRequirement>
-    {
-        readonly HttpActionContext _appContext;
-        readonly IHttpContextAccessor _contextAccessor;
+	public class ThePolicyAuthorizationHandler : AuthorizationHandler<ThePolicyRequirement>
+	{
+		readonly HttpActionContext _appContext;
+		readonly IHttpContextAccessor _contextAccessor;
 
-        public ThePolicyAuthorizationHandler(IHttpContextAccessor ca)
-        {
-            //_appContext = c;
-            _contextAccessor = ca;
-        }
+		public ThePolicyAuthorizationHandler(IHttpContextAccessor ca)
+		{
+			//_appContext = c;
+			_contextAccessor = ca;
+		}
 
-        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ThePolicyRequirement requirement)
-        {
-            var result = await requirement.isPass(_contextAccessor, context);
-            var api = _contextAccessor.HttpContext.Request.Path;
-            //var user = context.User;
-            //var user = context.User HttpContext.Items["User"];
-            System.Security.Claims.ClaimsPrincipal currentUser = context.User;
-            //bool isAuthroized = base.IsAuthorized(context);
-            //var currentUserName = currentUser.FindFirst(JwtRegisteredClaimNames.FamilyName).Value;
-            string Sub = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.Sub];
-            string UniqueName = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.UniqueName];
-            string FamilyName = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.FamilyName];
-            string GivenName = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.GivenName];
+		protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ThePolicyRequirement requirement)
+		{
+			var result = await requirement.isPass(_contextAccessor, context);
 
-            Claim username = context.User.Claims.FirstOrDefault(claim => claim.Type == Sub);
-            Claim fullnam = context.User.Claims.FirstOrDefault(claim => claim.Type == UniqueName);
-            Claim email = context.User.Claims.FirstOrDefault(claim => claim.Type == FamilyName);
-            Claim userId = context.User.Claims.FirstOrDefault(claim => claim.Type == GivenName);
-            if (result)
-                context.Succeed(requirement);
-            else
-                context.Fail();
-        }
-    }
+			//check IsAuthorized
+			var IsAuthenticated= context.User.Identity.IsAuthenticated;
+
+			//check userIsAnonymous
+			//var userIsAnonymous = false;
+			//var endpoint = _contextAccessor.HttpContext.GetEndpoint();
+			//if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() is object)
+			//{
+			//	userIsAnonymous = true;
+			//}
+
+			var api = _contextAccessor.HttpContext.Request.Path;
+			//var user = context.User;
+			//var user = context.User HttpContext.Items["User"];
+			System.Security.Claims.ClaimsPrincipal currentUser = context.User;
+			string APIName = _contextAccessor.HttpContext.Request.Path;
+
+			var mvcContext = context.Resource as AuthorizationHandlerContext;
+			var authorizationFilterContext = context.Resource as HttpContext;
+			//var descriptor = mvcContext?.ActionDescriptor as ControllerActionDescriptor;
+			//bool isAuthroized = base.IsAuthorized(context);
+			//var currentUserName = currentUser.FindFirst(JwtRegisteredClaimNames.FamilyName).Value;
+			string Sub = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.Sub];
+			string UniqueName = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.UniqueName];
+			string FamilyName = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.FamilyName];
+			string GivenName = JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.GivenName];
+
+			Claim username = context.User.Claims.FirstOrDefault(claim => claim.Type == Sub);
+			Claim fullnam = context.User.Claims.FirstOrDefault(claim => claim.Type == UniqueName);
+			Claim email = context.User.Claims.FirstOrDefault(claim => claim.Type == FamilyName);
+			Claim userId = context.User.Claims.FirstOrDefault(claim => claim.Type == GivenName);
+			if (result)
+				context.Succeed(requirement);
+			else
+				context.Fail();
+		}
+	}
 }
