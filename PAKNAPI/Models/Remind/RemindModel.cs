c21e6b424446054d7c087f08222f8a79908a542a
TranHungTbb <tranhung110398123@gmail.com>
2021-04-07 11:54:31 +0700

    filed,Hastag, remind

---
+++ PAKNAPI/Models/Remind/RemindModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAKNAPI.Models.Remind
+{
+    public class RMRemind
+    {
+        public int id { get; set; }
+        public int PetitionId { get; set; }
+        public string Content { get; set; }
+    }
+
+
+    public class RMFileAttach {
+        public int id { get; set; }
+        public int RemindId { get; set; }
+        public string FileAttach { get; set; }
+        public string Name { get; set; }
+        public int FileType { get; set; }
+    }
+
+    public class RMForward {
+        public int id {get; set;}
+        public long SenderId { get; set;}
+        public int SendOrgId { get; set;}
+        public int ReceiveOrgId { get; set;}
+        public DateTime DateSend { get; set;}
+        public int IsView { get; set;}
+    }
+
+    public class RMRemindView {
+        public RMRemind Model { get; set; }
+        public List<RMFileAttach> Files { get; set; }
+    }
+
+    public class RMRemindInsertRequest
+    {
+        public RMRemind Model { get; set; }
+        //public List<RMFileAttach> ltsFiles { get; set; }
+
+        public RMForward Forward { get; set; }
+
+        public IFormFileCollection Files { get; set; }
+    }
+}
