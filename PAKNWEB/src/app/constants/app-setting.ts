157d98c4a2e13f9669bd14a92753f526dedd250c
duongth <duongtl2@thanglonginc.com>
2021-04-20 14:40:58 +0700

    update

--- PAKNWEB/src/app/constants/app-setting.ts
+++ PAKNWEB/src/app/constants/app-setting.ts
@@ -1,8 +1,8 @@
 export class AppSettings {
-	public static HostingAddress = 'http://14.177.236.88'
+	public static HostingAddress = 'http://localhost'
 
-	public static API_ADDRESS = 'http://14.177.236.88:6161/api/'
-	public static API_DOWNLOADFILES = 'http://14.177.236.88:6161'
+	public static API_ADDRESS = 'http://localhost:51046/api/'
+	public static API_DOWNLOADFILES = 'http://localhost:51046'
 	public static VIEW_FILE = 'http://14.177.236.88:6062/DocViewer?fileurl='
 
 	// public static API_ADDRESS = 'http://14.177.236.88:6161/api/'
--- PAKNWEB/src/app/modules/dash-board/recommnendation-get-list/recommnendation-get-list.component.ts
+++ PAKNWEB/src/app/modules/dash-board/recommnendation-get-list/recommnendation-get-list.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { RecommendationService } from 'src/app/services/recommendation.service'
+import { ToastrService } from 'ngx-toastr'
+import { UserInfoStorageService } from 'src/app/commons/user-info-storage.service'
+import { MESSAGE_COMMON, PROCESS_STATUS_RECOMMENDATION, RECOMMENDATION_STATUS, RESPONSE_STATUS, STEP_RECOMMENDATION } from 'src/app/constants/CONSTANTS'
+declare var $: any
 
 @Component({
-  selector: 'app-recommnendation-get-list',
-  templateUrl: './recommnendation-get-list.component.html',
-  styleUrls: ['./recommnendation-get-list.component.css']
+	selector: 'app-recommnendation-get-list',
+	templateUrl: './recommnendation-get-list.component.html',
+	styleUrls: ['./recommnendation-get-list.component.css'],
 })
 export class RecommnendationGetListComponent implements OnInit {
+	constructor(private _service: RecommendationService, private _toast: ToastrService, private storeageService: UserInfoStorageService) {}
 
-  constructor() { }
+	listData: any[]
+	dataSearch: any = {}
+	totalRecords: number = 0
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getList()
+	}
 
+	getList() {
+		let request = {
+			Status: 5,
+			PageIndex: 1,
+			PageSize: 4,
+			UnitProcessId: this.storeageService.getUnitId(),
+			UserProcessId: this.storeageService.getUserId(),
+		}
+
+		this._service.recommendationGetListProcess(request).subscribe((response) => {
+			console.log(response)
+			if (response.success == RESPONSE_STATUS.success) {
+				if (response.result != null) {
+					this.listData = []
+					this.listData = response.result.MRRecommendationGetAllWithProcess
+					this.totalRecords = response.result.TotalCount
+				}
+			} else {
+				this._toast.error(response.message)
+			}
+		}),
+			(error) => {
+				console.log(error)
+				alert(error)
+			}
+	}
 }
