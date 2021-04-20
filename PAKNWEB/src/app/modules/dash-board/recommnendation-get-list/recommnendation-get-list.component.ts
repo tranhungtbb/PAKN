6a98cf804a607c0c0c749bc8adec227562b34738
duongth <duongtl2@thanglonginc.com>
2021-04-20 13:48:06 +0700

    update

---
+++ PAKNWEB/src/app/modules/dash-board/recommnendation-get-list/recommnendation-get-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommnendation-get-list',
+  templateUrl: './recommnendation-get-list.component.html',
+  styleUrls: ['./recommnendation-get-list.component.css']
+})
+export class RecommnendationGetListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
