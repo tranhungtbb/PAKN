7a1c5e064bfae7e73d82f754c0c94efec8ed2ce8
duongth <duongtl2@thanglonginc.com>
2021-06-11 18:02:33 +0700

    bản thảo email

---
+++ PAKNWEB/src/app/modules/email-sms/email-management/email-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-management',
+  templateUrl: './email-management.component.html',
+  styleUrls: ['./email-management.component.css']
+})
+export class EmailManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
