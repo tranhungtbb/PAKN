f4cfb2f8f918c448dc4b22b5be3e2c9a5627e333
nguyennn <nguyennn@thanglonginc.com>
2021-12-09 11:43:31 +0700

    fix: handle chat with avatar path

---
+++ PAKNAPI/Models/Chatbot/UserChatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAKNAPI.Models.Chatbot
+{
+    public class UserChatModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
